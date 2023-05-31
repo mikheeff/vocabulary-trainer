51366a70608ebff9fa4e9583f197a47d57c257a8
pavelmikheev <pavel@fluid.ch>
2023-05-31 22:44:12 +0400

    project init

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # vocabulary-trainer
-English Vocabulary Trainer
+
+## Project setup
+Install NodeJS LTS version: https://nodejs.org/en/
+### Install dependencies
+```
+yarn install
+```
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+### Compiles and minifies for production
+```
+yarn build
+```
+### Compiles for development
+```
+yarn dev
+```
+### Prettier watch for changes from the command line
+```
+yarn prettier-watch
+```
---
+++ webpack.config.ts
@@ -0,0 +1,52 @@
+import HtmlWebpackPlugin = require("html-webpack-plugin");
+import FileManagerPlugin from "filemanager-webpack-plugin";
+import { Configuration as WebpackConfiguration } from "webpack";
+import * as path from "path";
+import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
+
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
+
+const config: Configuration = {
+  entry: path.join(__dirname, "src", "index.ts"),
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "index.[contenthash].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-typescript"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+      filename: "index.html",
+    }),
+    new FileManagerPlugin({
+      events: {
+        onStart: {
+          delete: ["dist"],
+        },
+      },
+    }),
+  ],
+  devServer: {
+    hot: true
+  },
+};
+
+export default config;
